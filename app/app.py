7c3d8bc9107794c5403f86e06685d45fbe7bd038
Moran <128787888+colapstar@users.noreply.github.com>
2023-12-18 22:28:15 +0100

    test session/public database TMDB

--- app/app.py
+++ app/app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+import requests
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cine_match.db'
-db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'votre_secret_key'  # Changez ceci par une clé secrète réelle
 
-class Utilisateur(db.Model):
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class Utilisateur(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
@@ -17,43 +25,37 @@ class Utilisateur(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-class Film(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    titre = db.Column(db.String(80), nullable=False)
-    genre = db.Column(db.String(80), nullable=False)
+# Votre modèle Film et autres fonctions...
 
-films_fictifs = [
-    {"titre": "Film Action 1", "genre": "action"},
-    {"titre": "Film Comédie 1", "genre": "comédie"},
-    # Ajoutez d'autres films fictifs ici
-]
+@login_manager.user_loader
+def load_user(user_id):
+    return Utilisateur.query.get(int(user_id))
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
 
-@app.route('/submit_preferences', methods=['POST'])
-def submit_preferences():
-    genre = request.form['genre']
-    recommandations = recommander_films(genre)
-    return render_template('index.html', recommandations=recommandations)
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        new_user = Utilisateur(username=username)
-        new_user.set_password(password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('register.html')
+        user = Utilisateur.query.filter_by(username=username).first()
 
-def recommander_films(genre):
-    return [film for film in films_fictifs if film['genre'] == genre]
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('index'))
+
+        return 'Nom d’utilisateur ou mot de passe invalide.'
+
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+# Vos autres routes...
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
